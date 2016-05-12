2dea8d05021845691e045a4cf3562f02089b3e92
Your Name <you@example.com>
2016-05-12 07:04:02 +0000

    1

---
+++ helloworld/src/main/java/com/mycompany/helloworld/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public  static int testMeth(int number)
+    {
+        return number*number;
+    }	
+}
